95b8b3d537c6e09e24bcd5c84c69bce00fca3884
akaunderr <akaunderr@gmail.com>
2023-12-21 19:08:27 +0200

    added support of multisource

---
+++ dist/_ocxpak.def.php
@@ -0,0 +1,14 @@
+<?php
+
+define('MODVERS', '1.2.3');
+define('MODYEAR', '2021');
+
+define('AUTHOR', 'Andrii Burkatskyi');
+define('EMAIL', 'ocmod.space@gmail.com');
+
+define('MODLINK', 'https://www.ocmod.space/new-module');
+define('OCMLINK', 'https://www.opencart.com/index.php?route=marketplace/extension/info&extension_id=00000');
+define('GITLINK', 'https://github.com/ocmod-space/ocmod-new-module');
+define('XMLLINK', 'https://www.opencart.com/index.php?route=marketplace/extension&amp;filter_member=ocmod.space');
+
+define('LICLINK', 'https://raw.githubusercontent.com/ocmod-space/ocmod-new-module/main/EULA.txt');
---
+++ dist/_ocxpak/help.php
@@ -0,0 +1,13 @@
+<?php
+
+output('OCXPak - OpenCart eXtension Packer.');
+output('Using: php _ocxpak.php [-option]');
+output();
+output('Options:');
+output(' -z[0|1|2..N]' . "\t" . 'Zip extension or addon number [N] (see below).');
+output(' -v{23|3x|4x}' . "\t" . 'Target OC major version. Default value is "3x".');
+output(' -a' . "\t\t" . 'Make encrypted fcl-archive.');
+output(' -x' . "\t\t" . 'Extract encrypted fcl-archive into current directory.');
+output(' -l' . "\t\t" . 'List of files in encrypted fcl-archive.');
+output(' -h' . "\t\t" . 'Print this help.');
+output();
